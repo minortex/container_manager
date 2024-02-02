89b96ed78c3967a045435ef8c456dd54e4d54bda
texsd <tantexsd@126.com>
2024-02-02 22:10:17 +0800

    pull registery

--- script.sh
+++ script.sh
@@ -10,12 +10,19 @@ fi
 
 case $action in
   pull )
-    wget -O $name.tar $path >> /dev/null 2>$1
+    docker pull $name
+    name=${2//\//_}
+    docker save -o $name.tar $2
+#    wget -O $name.tar $path >> /dev/null 2>$1
     if [ $? -eq 0 ]; then
       echo "Successfully Downloaded. Begin exacting..."
-      mkdir $name
-      tar -xvf $name.tar -C $name >> /dev/null 2>$1
-      rm $name.tar
+      mkdir $path/$name
+      tar -xvf $path/$name.tar -C $path/$name >> /dev/null 2>$1
+      rm $path/$name.tar
+      cd $path/$name
+      mkdir rootfs
+      docker export $(docker create busybox) | tar -C rootfs -xvf -
+      runc spec
       echo "Done."
     else
 	  echo "Failed to download file."
